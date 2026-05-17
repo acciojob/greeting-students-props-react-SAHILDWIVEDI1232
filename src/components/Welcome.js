cd8d225356114a2a036d755290eb582f65f47ec3
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-17 22:44:05 +0530

    Add Welcome component with greeting message
    Implement Welcome component to greet users.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+function Welcome({name}){
+  return(
+    <h1>
+    Hey !{name}</h1>
+  <h2>
+    Welcome to School.
+     <p>We are happy to have you here.</p>
+    </h2>
+  export deafault Welcome
